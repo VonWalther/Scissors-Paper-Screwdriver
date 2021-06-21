1e3ca35859b39a3b6b14b0252dc5d36b94aa3c98
VonWalther <carl.walther@sbcglobal.net>
2021-06-21 11:15:54 -0500

    Initial Commit

---
+++ Scissors, Paper; Screwdriver/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Scissors, Paper; Screwdriver
+//
+//  Created by Carl Walther on 6/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Scissors, Paper; Screwdriver/Scissors__Paper__ScrewdriverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scissors__Paper__ScrewdriverApp.swift
+//  Scissors, Paper; Screwdriver
+//
+//  Created by Carl Walther on 6/21/21.
+//
+
+import SwiftUI
+
+@main
+struct Scissors__Paper__ScrewdriverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
